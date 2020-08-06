60e092d20c6d82f257a6b5a2163bd704e25ee2dc
Kedrick06 <bfloblk@gmail.com>
2020-08-06 10:41:39 -0500

    commit

--- nautilus/src/App.js
+++ nautilus/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hero from './Components/Hero'
@@ -7,9 +7,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    
         <div>
+        <img src='C:\Users\bflob\Desktop\nautilus_mockup\nautilus\assets\matches.png' alt="pic"/>
+          <p>Issue 079</p>
+            <h1>Catalysts</h1>
+            <p>Agents of change</p>
+                <button>Read More</button>
+          <Fragment>
           <Hero />
+
+          </Fragment>
         </div>
 
       </header>

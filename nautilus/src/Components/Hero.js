86cf7e1a5d4bcb8a3320acfb0fa459e34f1a2385
Kedrick06 <bfloblk@gmail.com>
2020-08-07 09:24:06 -0500

    commit

--- nautilus/src/Components/Hero.js
+++ nautilus/src/Components/Hero.js
@@ -9,7 +9,7 @@ class Home extends Component{
 
          <div className="background">
          <div className="text">
-           <h4 className="_issue">ISSUE 079</h4>
+           <h4 className="mag">ISSUE 079</h4>
            <h2 className="h2">Catalyst</h2>
            <h5>Agents of change</h5>
            <a  href="" className="_link">Read More</a>
